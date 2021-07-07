99251f7a3896996d7a844c4f23955fdcba876fed
Turtle <b05902009@ntu.edu.tw>
2021-07-07 18:06:58 +0800

    merge meteoroid into play scene, add character bound

--- Assets/Scripts/Play/DogeController.cs
+++ Assets/Scripts/Play/DogeController.cs
@@ -10,30 +10,68 @@ public class DogeController:MonoBehaviour
     public float CameraLowerBound;
     public float CameraLeftBound;
     public float CameraRightBound;
+    public float DogeUpperBound;
+    public float DogeLowerBound;
+    public float DogeLeftBound;
+    public float DogeRightBound;
 
     private List<DogeCommand> mFrameCommands;
 
+    private bool mIsPlaying;
+
+    private const float SpeedConst = 0.1f;
+    
     private void Start()
     {
         DogeCamera.transform.localPosition = new Vector3(0, 0, -5);
-        gameObject.transform.localPosition = new Vector3(-6.64f, -0.25f, 3.46f);
+        gameObject.transform.localPosition = new Vector3(0, 0, 0);
+        mIsPlaying = false;
+    }
+
+    public void StartGame()
+    {
         mFrameCommands = new List<DogeCommand>();
+        mIsPlaying = true;
+    }
+
+    public List<DogeCommand> EndGame()
+    {
+        mIsPlaying = false;
+        return mFrameCommands;
     }
 
     private void Update()
     {
+        if (!mIsPlaying) return;
+        
         var command = new DogeCommand();
         var transformDirection = InputDirection();
         var cameraPosition = DogeCamera.transform.localPosition;
         var cameraPositionAfter = cameraPosition + new Vector3(transformDirection.x * -1, transformDirection.y * -1, 0);
         if (cameraPositionAfter.x > CameraLeftBound &&
-            cameraPositionAfter.x < CameraRightBound &&
-            cameraPositionAfter.y < CameraUpperBound &&
+            cameraPositionAfter.x < CameraRightBound)
+            DogeCamera.transform.localPosition += new Vector3(transformDirection.x * -1, 0, 0);
+        if (cameraPositionAfter.y < CameraUpperBound &&
             cameraPositionAfter.y > CameraLowerBound)
-            DogeCamera.transform.localPosition = cameraPositionAfter;
-        gameObject.transform.localPosition += transformDirection;
-        command.Type |= SerializeDirection(transformDirection.x, transformDirection.y);
-        
+            DogeCamera.transform.localPosition += new Vector3(0, transformDirection.y * -1, 0);
+
+        var dogePositionAfter = gameObject.transform.localPosition + transformDirection;
+        if (dogePositionAfter.x > DogeLeftBound &&
+            dogePositionAfter.x < DogeRightBound)
+        {
+            gameObject.transform.localPosition += new Vector3(transformDirection.x, 0, 0);
+            command.Type |= transformDirection.x > 0 ? DogeCommandType.Right :
+                transformDirection.x < 0 ? DogeCommandType.Left : DogeCommandType.None;
+        }
+
+        if (dogePositionAfter.y < DogeUpperBound &&
+            dogePositionAfter.y > DogeLowerBound)
+        {
+            gameObject.transform.localPosition += new Vector3(0, transformDirection.y, 0);
+            command.Type |= transformDirection.y > 0 ? DogeCommandType.Up :
+                transformDirection.y < 0 ? DogeCommandType.Down : DogeCommandType.None;
+        }
+
         if (Input.GetMouseButtonUp(0))
         {
             var mouseViewport = DogeCamera.ScreenToViewportPoint(Input.mousePosition);
@@ -50,19 +88,19 @@ public class DogeController:MonoBehaviour
         var direction = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
         {
-            direction += Vector3.up * 0.02f;
+            direction += Vector3.up * SpeedConst;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            direction += Vector3.down * 0.02f;
+            direction += Vector3.down * SpeedConst;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            direction += Vector3.left * 0.02f;
+            direction += Vector3.left * SpeedConst;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            direction += Vector3.right * 0.02f;
+            direction += Vector3.right * SpeedConst;
         }
 
         return direction;
---
+++ Assets/Scripts/Play/PlayManager.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    #region Singleton
+    private static PlayManager mInstance;
+    public static PlayManager Instance
+    {
+        get
+        {
+            if (mInstance == null)
+            {
+                mInstance = FindObjectOfType<PlayManager>();
+            }
+
+            return mInstance;
+        }
+    }
+    #endregion
+
+    public DogeController MainCharacterController;
+
+    private void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        MainCharacterController.StartGame();
+    }
+
+    public void StopGame()
+    {
+        MainCharacterController.EndGame();
+    }
+    
+}
